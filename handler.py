01f1f21b44f3829db7838faa69584f4fd117da26
Rafael Zanetti <rfzanetti@gmail.com>
2021-09-07 01:18:09 -0300

    Initial commit

---
+++ handler.py
@@ -0,0 +1,13 @@
+from score_extraction import extract_scores
+from scorekeeping import update_game_score
+
+
+def handler(event, context):
+    scores = extract_scores()
+
+    for score in scores:
+        update_game_score(game_score=score)
+
+
+if __name__ == "__main__":
+    handler({}, "")
---
+++ score_extraction.py
@@ -0,0 +1,65 @@
+import requests
+
+from bs4 import BeautifulSoup
+
+SCOREBOARD_URL = "https://www.cbssports.com/nfl/scoreboard/"
+
+GAME_NOT_STARTED_STATUS = "NOT_STARTED"
+GAME_LIVE_STATUS = "LIVE"
+GAME_FINAL_STATUS = "FINAL"
+
+
+def extract_scores():
+    page = requests.get(SCOREBOARD_URL)
+    soup = BeautifulSoup(page.content, "html.parser")
+
+    game_cards = soup.find_all(class_="single-score-card")
+    games = [parse_game(game) for game in game_cards]
+
+    return [game for game in games if game]
+
+
+def parse_game(game):
+
+    score = game_score(game)
+    status = game_status(game)
+
+    if score and status:
+        return {
+            **score,
+            **status
+        }
+
+
+def game_score(game):
+    obj = dict()
+
+    teams = game.find_all(class_="team helper-team-name")
+    scores = game.find_all(class_="total-score")
+
+    if teams:
+        obj["away"] = teams[0].get_text()
+        obj["home"] = teams[1].get_text()
+
+        if scores:
+            obj["away_score"] = scores[0].get_text()
+            obj["home_score"] = scores[1].get_text()
+        else:
+            obj["away_score"] = 0
+            obj["home_score"] = 0
+
+    return obj
+
+
+def game_status(game):
+    status = game.find(class_="game-status")
+
+    if not status:
+        return
+
+    if "pregame" in status["class"]:
+        return {"status": GAME_NOT_STARTED_STATUS}
+    elif "postgame" in status["class"]:
+        return {"status": GAME_FINAL_STATUS}
+    else:
+        return {"status": GAME_LIVE_STATUS}
